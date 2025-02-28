2743a47d70d7f12b970c2819d666f6f0c25900d7
17nim <17nim.m@gmail.com>
2025-02-28 18:01:07 +0700

    Remove unnecessary floating point

--- function.py
+++ function.py
@@ -10,45 +10,45 @@ def openPetrunko () :
     time.sleep(1)  # รอให้แอปเปิดก่อน
                 #pyautogui.click(x=794.0078125, y=842.26171875)  # ปรับตำแหน่งให้ตรงกับโฟลเดอร์
     time.sleep(1)  # รอให้แอปเปิดก่อน
-    pyautogui.click(x=153.5078125, y=220.4765625)  # ปรับตำแหน่งให้ตรงกับโฟลเดอร์
+    pyautogui.click(x=153, y=220)  # ปรับตำแหน่งให้ตรงกับโฟลเดอร์
     time.sleep(1)  # รอให้แอปเปิดก่อน
-    pyautogui.click(x=143.34765625, y=237.89453125)  # ปรับตำแหน่งให้ตรงกับโฟลเดอร์
+    pyautogui.click(x=143, y=237)  # ปรับตำแหน่งให้ตรงกับโฟลเดอร์
     time.sleep(1)
-    pyautogui.doubleClick(x=502.5, y=156.59765625)  # ปรับตำแหน่งให้ตรงกับโฟลเดอร์
+    pyautogui.doubleClick(x=502, y=156)  # ปรับตำแหน่งให้ตรงกับโฟลเดอร์
     time.sleep(1)  # รอให้แอปเปิดก่อน
                 
     pyautogui.hotkey("shift", "space")
     pyautogui.press("space")  # กดปุ่ม Play
-    return "Hand Up (FunkonAut).."
+    return "Hands Up (FunkonAut).."
 
 def openFunkonaut  ():
     subprocess.run(["open", "-a", "SoundSwitch"])
     time.sleep(1)  # รอให้แอปเปิดก่อน
                 #pyautogui.click(x=794.0078125, y=842.26171875)  # ปรับตำแหน่งให้ตรงกับโฟลเดอร์
     time.sleep(1)  # รอให้แอปเปิดก่อน
-    pyautogui.click(x=153.5078125, y=220.4765625)  # ปรับตำแหน่งให้ตรงกับโฟลเดอร์
+    pyautogui.click(x=153, y=220)  # ปรับตำแหน่งให้ตรงกับโฟลเดอร์
     time.sleep(1)  # รอให้แอปเปิดก่อน
-    pyautogui.click(x=143.34765625, y=237.89453125)  # ปรับตำแหน่งให้ตรงกับโฟลเดอร์
+    pyautogui.click(x=143, y=237)  # ปรับตำแหน่งให้ตรงกับโฟลเดอร์
     time.sleep(1)
-    pyautogui.doubleClick(x=411.3359375, y=132.29296875)
+    pyautogui.doubleClick(x=411, y=132)
     time.sleep(1)  # รอให้แอปเปิดก่อน
                 
     pyautogui.hotkey("shift", "space")
     pyautogui.press("space")  # กดปุ่ม Play
-    return "titanic pose (Petrunko).."
+    return "Wide Hands Pose (Petrunko).."
 
 def openAstroFunk ():
     subprocess.run(["open", "-a", "SoundSwitch"])
     time.sleep(1)  # รอให้แอปเปิดก่อน
                 #pyautogui.click(x=794.0078125, y=842.26171875)  # ปรับตำแหน่งให้ตรงกับโฟลเดอร์
     time.sleep(1)  # รอให้แอปเปิดก่อน
-    pyautogui.click(x=153.5078125, y=220.4765625)  # ปรับตำแหน่งให้ตรงกับโฟลเดอร์
+    pyautogui.click(x=153, y=220)  # ปรับตำแหน่งให้ตรงกับโฟลเดอร์
     time.sleep(1)  # รอให้แอปเปิดก่อน
-    pyautogui.click(x=143.34765625, y=237.89453125)  # ปรับตำแหน่งให้ตรงกับโฟลเดอร์
+    pyautogui.click(x=143, y=237)  # ปรับตำแหน่งให้ตรงกับโฟลเดอร์
     time.sleep(1)
-    pyautogui.doubleClick(x=371.734375, y=179.43359375)
+    pyautogui.doubleClick(x=371, y=179)
     time.sleep(1)  # รอให้แอปเปิดก่อน
                 
     pyautogui.hotkey("shift", "space")
     pyautogui.press("space")  # กดปุ่ม Play
-    return ".."
+    return "(AstroFunk).."
